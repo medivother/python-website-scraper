de469791554643c386c07b1f49fae5b0dedf8fb2
medivother <99642347+medivother@users.noreply.github.com>
2022-03-21 23:02:35 +0100

    Create scraper.py
    This is a very basic idea of how to log in into a website using requests

---
+++ scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+login_url = ['xxxx']
+
+payload = {
+    'password': 'xxxxxxxxxxxxxxx',
+    'username': 'xxxxxxxxxxxxxxx'
+}
+with requests.session() as s:       #this will maintain the log in so that you can do multiple requests to the secured pages
+    s.post(login_url,data=payload)  
